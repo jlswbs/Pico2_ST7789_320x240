cb3dad0e18d0988d5eb4717816d046fb75284c22
jlswbs <jlswbs@users.noreply.github.com>
2024-12-13 19:18:40 +0100

    Add files via upload

---
+++ 2D_PerlinNoise/2D_PerlinNoise.ino
@@ -0,0 +1,236 @@
+// 2D Perlin noise //
+
+#include "hardware/structs/rosc.h"
+#include "st7789_lcd.pio.h"
+
+#define PIN_DIN   11
+#define PIN_CLK   10
+#define PIN_CS    9
+#define PIN_DC    8
+#define PIN_RESET 12
+#define PIN_BL    13
+#define KEY_A     15
+
+PIO pio = pio0;
+uint sm = 0;
+uint offset = pio_add_program(pio, &st7789_lcd_program);
+
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+#define WIDTH   240
+#define HEIGHT  320
+#define SCR     (WIDTH*HEIGHT)
+#define ITER    6
+
+uint16_t color565(uint8_t red, uint8_t green, uint8_t blue) { return ((red & 0xF8) << 8) | ((green & 0xFC) << 3) | (blue >> 3); }
+float randomf(float minf, float maxf) {return minf + (rand()%(1UL << 31))*(maxf - minf) / (1UL << 31);}
+
+  uint16_t image;
+  int seed = 0;
+  float freq = 0.01f;
+
+static int hash[] = { 208,34,231,213,32,248,233,56,161,78,24,140,71,48,140,254,245,255,247,247,40,
+                     185,248,251,245,28,124,204,204,76,36,1,107,28,234,163,202,224,245,128,167,204,
+                     9,92,217,54,239,174,173,102,193,189,190,121,100,108,167,44,43,77,180,204,8,81,
+                     70,223,11,38,24,254,210,210,177,32,81,195,243,125,8,169,112,32,97,53,195,13,
+                     203,9,47,104,125,117,114,124,165,203,181,235,193,206,70,180,174,0,167,181,41,
+                     164,30,116,127,198,245,146,87,224,149,206,57,4,192,210,65,210,129,240,178,105,
+                     228,108,245,148,140,40,35,195,38,58,65,207,215,253,65,85,208,76,62,3,237,55,89,
+                     232,50,217,64,244,157,199,121,252,90,17,212,203,149,152,140,187,234,177,73,174,
+                     193,100,192,143,97,53,145,135,19,103,13,90,135,151,199,91,239,247,33,39,145,
+                     101,120,99,3,186,86,99,41,237,203,111,79,220,135,158,42,30,154,120,67,87,167,
+                     135,176,183,191,253,115,184,21,233,58,129,233,142,39,128,211,118,137,139,255,
+                     114,20,218,113,154,27,127,246,250,1,8,198,250,209,92,222,173,21,88,102,219 };
+
+int noise2(int x, int y){
+
+  int tmp = hash[(y + seed) % 256];
+  return hash[(tmp + x) % 256];
+
+}
+
+float lin_inter(float x, float y, float s){ return x + s * (y - x); }
+
+float smooth_inter(float x, float y, float s){ return lin_inter(x, y, s * s * (3 - 2 * s)); }
+
+float noise2d(float x, float y){
+  
+  int x_int = x;
+  int y_int = y;
+  float x_frac = x - x_int;
+  float y_frac = y - y_int;
+  int s = noise2(x_int, y_int);
+  int t = noise2(x_int + 1, y_int);
+  int u = noise2(x_int, y_int + 1);
+  int v = noise2(x_int + 1, y_int + 1);
+  float low = smooth_inter(s, t, x_frac);
+  float high = smooth_inter(u, v, x_frac);
+
+  return smooth_inter(low, high, y_frac);
+
+}
+
+float perlin2d(float x, float y, float freq, int depth){
+  
+  float xa = x * freq;
+  float ya = y * freq;
+  float amp = 1.0;
+  float fin = 0;
+  float div = 0.0;
+
+  for (int i = 0; i < depth; i++){    
+    div += 256 * amp;
+    fin += noise2d(xa, ya) * amp;
+    amp /= 2;
+    xa *= 2;
+    ya *= 2;
+  }
+
+  return fin / div;
+
+}
+
+#define SERIAL_CLK_DIV 2.f
+
+static const uint8_t st7789_init_seq[] = {
+        1, 20, 0x01,                        // Software reset
+        1, 10, 0x11,                        // Exit sleep mode
+        2, 2, 0x3a, 0x55,                   // Set colour mode to 16 bit
+        2, 0, 0x36, 0x00,                   // Set MADCTL: row then column, refresh is bottom to top ????
+        5, 0, 0x2a, 0x00, 0x00, 0x00, 0xf0, // CASET: column addresses from 0 to 240 (f0)
+        5, 0, 0x2b, 0x00, 0x00, 0x01, 0x40, // RASET: row addresses from 0 to 320
+        1, 2, 0x21,                         // Inversion on, then 10 ms delay (supposedly a hack?)
+        1, 2, 0x13,                         // Normal display on, then 10 ms delay
+        1, 2, 0x29,                         // Main screen turn on, then wait 500 ms
+        0                                   // Terminate list
+};
+
+static inline void lcd_set_dc_cs(bool dc, bool cs) {
+
+  sleep_us(1);
+  gpio_put_masked((1u << PIN_DC) | (1u << PIN_CS), !!dc << PIN_DC | !!cs << PIN_CS);
+  sleep_us(1);
+
+}
+
+static inline void lcd_write_cmd(PIO pio, uint sm, const uint8_t *cmd, size_t count) {
+
+  st7789_lcd_wait_idle(pio, sm);
+  lcd_set_dc_cs(0, 0);
+  st7789_lcd_put(pio, sm, *cmd++);
+  if (count >= 2) {
+  st7789_lcd_wait_idle(pio, sm);
+  lcd_set_dc_cs(1, 0);
+  for (size_t i = 0; i < count - 1; ++i) st7789_lcd_put(pio, sm, *cmd++);
+  }
+  st7789_lcd_wait_idle(pio, sm);
+  lcd_set_dc_cs(1, 1);
+
+}
+
+static inline void lcd_init(PIO pio, uint sm, const uint8_t *init_seq) {
+
+  const uint8_t *cmd = init_seq;
+  while (*cmd) {
+  lcd_write_cmd(pio, sm, cmd + 2, *cmd);
+  sleep_ms(*(cmd + 1) * 5);
+  cmd += *cmd + 2;
+  }
+}
+
+static inline void st7789_start_pixels(PIO pio, uint sm) {
+
+  uint8_t cmd = 0x2c; // RAMWR
+  lcd_write_cmd(pio, sm, &cmd, 1);
+  lcd_set_dc_cs(1, 0);
+
+}
+
+static inline void seed_random_from_rosc(){
+  
+  uint32_t random = 0;
+  uint32_t random_bit;
+  volatile uint32_t *rnd_reg = (uint32_t *)(ROSC_BASE + ROSC_RANDOMBIT_OFFSET);
+
+  for (int k = 0; k < 32; k++) {
+    while (1) {
+      random_bit = (*rnd_reg) & 1;
+      if (random_bit != ((*rnd_reg) & 1)) break;
+    }
+
+    random = (random << 1) | random_bit;
+  }
+
+  srand(random);
+}
+
+void rndrule(){
+
+  st7789_start_pixels(pio, sm);
+
+  for (int i=0; i<SCR; i++) {
+
+    image = BLACK;
+    st7789_lcd_put(pio, sm, image >> 8);
+    st7789_lcd_put(pio, sm, image & 0xff);
+
+  }
+
+  freq = randomf(0.005f, 0.05f);
+  seed = rand()%256;
+
+}
+
+void setup() {
+
+  st7789_lcd_program_init(pio, sm, offset, PIN_DIN, PIN_CLK, SERIAL_CLK_DIV);
+
+  gpio_init(PIN_CS);
+  gpio_init(PIN_DC);
+  gpio_init(PIN_RESET);
+  gpio_init(PIN_BL);
+  gpio_init(KEY_A);
+  gpio_set_dir(PIN_CS, GPIO_OUT);
+  gpio_set_dir(PIN_DC, GPIO_OUT);
+  gpio_set_dir(PIN_RESET, GPIO_OUT);
+  gpio_set_dir(PIN_BL, GPIO_OUT);
+  gpio_set_dir(KEY_A, GPIO_IN);
+
+  gpio_put(PIN_CS, 1);
+  gpio_put(PIN_RESET, 1);
+  lcd_init(pio, sm, st7789_init_seq);
+  gpio_put(PIN_BL, 1);
+  gpio_pull_up(KEY_A);
+
+  seed_random_from_rosc();
+
+  rndrule();
+  
+}
+
+void loop() {
+
+  if (gpio_get(KEY_A) == false) rndrule();
+
+  for(int y=0; y<HEIGHT; y++) {
+
+    for(int x=0; x<WIDTH; x++) {
+      
+      uint8_t coll = 255.0f * perlin2d(x, y, freq, ITER);
+      image = color565(coll, coll, coll);
+
+      st7789_lcd_put(pio, sm, image >> 8);
+      st7789_lcd_put(pio, sm, image & 0xff);
+ 
+    }
+
+  }
+
+}
